d25484661cebeaaa679eb7a317821315671ada9b
Zhannur <jannuraidynuly@gmail.com>
2021-11-05 21:02:57 +0600

    [FEATURE] init method

---
+++ src/db/database.py
@@ -0,0 +1,9 @@
+import psycopg2
+
+
+class DataBaseJob:
+    def __init__(self, host, user, password, db_name):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.db_name = db_name
