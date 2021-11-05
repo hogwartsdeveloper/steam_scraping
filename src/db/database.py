9b3239d50b7f11941a3ffdb0bd94e7bc0402c979
Zhannur <jannuraidynuly@gmail.com>
2021-11-05 21:14:02 +0600

    [FEATURE] method for connect db

--- src/db/database.py
+++ src/db/database.py
@@ -2,8 +2,26 @@ import psycopg2
 
 
 class DataBaseJob:
-    def __init__(self, host, user, password, db_name):
+    def __init__(self, host, user, password, database):
         self.host = host
         self.user = user
         self.password = password
-        self.db_name = db_name
+        self.database = database
+
+    def connect(self):
+        try:
+            connection = psycopg2.connect(
+                host=self.host,
+                user=self.user,
+                password=self.password,
+                database=self.database
+            )
+            print("[INFO] PostgreSQL connection successful")
+        except Exception as _ex:
+            print("[Error] Error while working with PostgreSQL", _ex)
+
+        yield connection
+
+        if connection:
+            connection.close()
+            print("[INFO] PostgreSQL connection closed")
