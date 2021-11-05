e745ac25f02edf9b1f66f76aebe1506cf505933b
Zhannur <jannuraidynuly@gmail.com>
2021-11-05 22:43:09 +0600

    [FEATURE] method for create table games

--- src/db/database.py
+++ src/db/database.py
@@ -7,7 +7,6 @@ class DataBaseJob:
         self.user = user
         self.password = password
         self.database = database
-        self.connection = None
 
     def connect(self):
         try:
@@ -22,16 +21,46 @@ class DataBaseJob:
         except Exception as _ex:
             print("[Error] Error while working with PostgreSQL", _ex)
 
-    def create_table_genre(self):
+    def create_table_genres(self):
+        connection = None
         try:
-            with self.connect().cursor() as cursor:
+            connection = self.connect()
+            with connection.cursor() as cursor:
                 cursor.execute(
                     """
                         CREATE TABLE genres(
-                            genre_id SERIAL PRIMARY KEY,
-                            name VARCHAR(50) NOT NULL);
+                            id SERIAL PRIMARY KEY,
+                            name VARCHAR(50) NOT NULL
+                        );
                     """
                 )
+                connection.commit()
             print("[INFO] Table genres created successful")
         except Exception as _ex:
             print("[ERROR] Table not created", _ex)
+        finally:
+            if connection:
+                connection.close()
+                print("[INFO] PostgreSQL connection closed")
+
+    def create_table_games(self):
+        connection = None
+        try:
+            connection = self.connect()
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    """
+                        CREATE TABLE games(
+                            game_id SERIAL PRIMARY KEY,
+                            name VARCHAR(50) NOT NULL,
+                            genre_id INTEGER REFERENCES genres(id) ON DELETE CASCADE
+                        );
+                    """
+                )
+                print("[INFO] Table games created successful")
+        except Exception as _ex:
+            print("[ERROR] Table not created", _ex)
+        finally:
+            if connection:
+                connection.close()
+                print("[INFO] PostgreSQL connection closed")
