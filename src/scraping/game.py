9424422ce22f275705e5aca6a3b74be6f08f4856
Zhannur <jannuraidynuly@gmail.com>
2021-11-06 16:41:56 +0600

    [FEATURE] method for connect

--- src/scraping/game.py
+++ src/scraping/game.py
@@ -1,2 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+
 class SteamGameScraping:
-    pass
+    def __init__(self, genre, link):
+        self.genre = genre
+        self.link = link
+
+    def connect(self):
+        header = {
+            "user-agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:94.0) Gecko/20100101 Firefox/94.0"
+        }
+        response = requests.get(self.link + "#p=0&tab=TopSellers", headers=header)
+
+        with open(f"src/scraping/html/steam_{self.genre}.html", "w") as file:
+            file.write(response.text)
